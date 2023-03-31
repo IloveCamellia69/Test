07f7ae065d87a1950688e2aeaabed12d93332dc6
Jacky <7nkn92n7s34@gmail.com>
2023-03-31 22:26:09 +1300

    Added and completed week 6

--- javascript/cursor.js
+++ javascript/cursor.js
@@ -2,8 +2,9 @@ var virtical = document.getElementById("virtical");
 var horizontal = document.getElementById("horizontal");
 
 document.addEventListener("mousemove", function(event) {
-  var x = event.clientX;
-  var y = event.clientY
+  let x = event.clientX;
+      y = event.clientY
+      
   virtical.style.left = x + "px";
   horizontal.style.top = y + "px";
 });
